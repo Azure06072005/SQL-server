d4fe7b3c79d5c1f8a3a102c13b670f47915a159e
unknown <23520820@gm.uit.edu.vn>
2024-08-17 20:44:36 +0700

    updated

---
+++ Code/Basic_exercise.sql
@@ -0,0 +1,75 @@
+﻿-------------------------------------------Exercise--------------------------------------
+--Hãy cho biết những khách hàng nào đã đặt nhiều hơn 20 đơn hàng, sắp xếp theo thứ tự tổng số 
+--đơn hàng giảm dần 
+select CustomerID as "Khách hàng",
+	count(OrderID) as "Đơn hàng"
+from Orders
+group by CustomerID
+having count(OrderID) > 20
+order by count(OrderID) desc
+
+--Lọc ra các nhân viên (EmployeeID) có tổng số đơn hàng lớn hơn hoặc bằng 100, sắp xếp theo 
+--tổng số đơn hàng giảm dần
+select EmployeeID as "Nhân viên",
+	count(OrderID) as "Đơn hàng" 
+from Orders
+group by EmployeeID
+having count(OrderID) >= 100
+order by count(OrderID) desc
+
+--Hãy cho biết những thể loại nào (CategoryID) có số sản phẩm khác nhau lớn hơn 11
+select distinct CategoryID, COUNT(ProductID) as "Sản phẩm"
+from Products
+group by CategoryID
+having count(ProductID) > 11 
+
+--Hãy cho biết những thể loại nào (CategoryID) có tổng số lượng sản phẩm trong kho	
+--(UnitInStock) lớn hơn 350
+select CategoryID as "Thể loại",
+	sum(UnitsInStock) as "Tổng số lượng sản phẩm" 
+from Products
+group by CategoryID
+having sum(UnitsInStock) > 350
+
+--Hãy cho biết quốc gia nào có nhiều hơn 7 khách hàng
+select Country as "Quốc gia",
+	count(CustomerID) as "Khách hàng" 
+from Customers
+group by Country
+having count(CustomerID) > 7
+
+--Hãy cho biết quốc gia nào có nhiều hơn 7 đơn hàng
+select ShipCountry as "Quốc gia",
+	count(OrderID) as "Đơn hàng"
+from Orders
+group by ShipCountry
+having count(OrderID) > 7
+
+--Hãy cho biết những ngày nào có nhiều hơn 5 đơn hàng được giao,
+--sắp xếp tăng dần theo ngày giao hàng
+select ShippedDate as "Ngày giao hàng",
+	count(OrderID) as "Đơn hàng" 
+from Orders
+where ShippedDate is not null
+group by ShippedDate
+having count(OrderID) > 5
+order by ShippedDate asc
+
+--Hãy cho biết những quốc gia bắt đầu bằng chữ 'A' hoặc 'G'
+--và có số lượng đơn hàng lớn hơn 29
+select ShipCountry as "Quốc gia",
+	count(OrderID) as "Đơn hàng" 
+from Orders
+where ShipCountry like '[A,G]%'
+group by ShipCountry
+having count(OrderID) > 29
+order by ShipCountry asc
+
+--Hãy cho biết những thành phố nào có số lượng đơn hàng được giao là khác 1 và 2,
+--đặt hàng từ ngày '1997-04-01' đến '1997-08-31'
+select ShipCity as "Thành phố",
+	count(OrderID) as "Đơn hàng"
+from Orders
+where ShippedDate between '1997-04-01' and '1997-08-31'
+group by ShipCity
+having count(OrderID) <> 1 and count(OrderID) <> 2
---
+++ Code/Having.sql
@@ -0,0 +1,64 @@
+﻿------------------------------------------Having-----------------------------------------
+--select column_name(s)
+--from table_name
+--where condition
+--group by column_name(s)
+--having condition
+--order by column_name(s)
+--Lọc dữ liệu sau group by, có vai trò xét các điều kiện của sum, count, avg
+------------------------------------------------------------------------------------------
+--hãy cho biết những khách hàng đặt nhiều hơn 20 đơn hàng, sắp xếp theo thứ tự tổng số 
+--đơn hàng giảm dần
+select CustomerID, count(OrderID) as TotalOrders
+from Orders
+group by CustomerID
+having count(OrderID) > 20
+order by count(OrderID) desc
+
+--Lọc ra những nhà cung cấp sản phẩm có (UnitInStock) > 30, và có trung bình đơn giá (UnitPrice)
+--có giá trị dưới 50
+select SupplierID, 
+sum(UnitsInStock) as "Sản phẩm có trong kho",
+avg(UnitPrice) as "Trung bình giá"
+from Products
+group by SupplierID
+having sum(UnitsInStock) > 30 and avg(UnitPrice) < 50
+
+--Cho biết tổng số tiền vận chuyển của từng tháng, trong nửa năm sau của năm 1996	
+--sắp xếp theo tháng tăng dần
+select MONTH(OrderDate) as "month", sum(Freight) "Tổng số tiền vận chuyển"
+from Orders
+where OrderDate between '1996-07-01' and '1996-12-31'
+group by MONTH(OrderDate)
+--having MONTH(OrderDate) > 6 and YEAR(OrderDate) > 1996
+order by month(OrderDate) asc
+
+--Cho biết tổng số tiền vận chuyển của từng tháng, trong nửa năm sau của năm 1996	
+--sắp xếp theo tháng tăng dần và tổng tiền vận chuyển lớn hơn 1000$
+select month(ShippedDate) as "month", sum(Freight) as "Tổng tiền vận chuyển"
+from Orders
+where ShippedDate between '1996-07-01' and '1996-12-31' 
+group by month(ShippedDate)
+having sum(Freight) > 1000
+order by sum(Freight) asc
+
+-----------------------------------------------------------------------------------------------
+--Lọc ra nhưng thành phố có số lượng đơn hàng > 16 và sắp xếp theo tổng số lượng giảm dần 
+select ShipCity as "Thành phố",
+	count(OrderID) as "Mã đơn hàng"
+from Orders
+group by ShipCity
+having COUNT(OrderID) > 16
+order by count(OrderID) desc
+
+-- Bài tập: Lọc ra những khách hàng từ Đức (Germany) có tổng số tiền vận
+--chuyển (Freight) lớn hơn $1000, và sắp xếp theo tổng số tiền vận 
+--chuyển giảm dần.
+select CustomerID as "Khách hàng",
+	sum(Freight) as "Tổng số tiền vận chuyển",
+	ShipCountry as "Quốc gia"
+from Orders
+where ShipCountry = 'Germany'
+group by CustomerID, ShipCountry
+having sum(Freight) > 1000
+order by sum(Freight) desc
