b31b39700d375f671d5770b811a78846ee6782ed
unknown <23520820@gm.uit.edu.vn>
2024-08-24 21:35:09 +0700

    updated

---
+++ Code/Join_exercise.sql
@@ -0,0 +1,45 @@
+﻿--------------------------------------Join exercises-------------------------------------
+--Bài tập 1: inner join
+--Liệt kê tên sản phẩm và tên nhà cung cấp của các sản phẩm đã được đặt hàng trong bảng
+--"Order Details". 
+select distinct od.ProductID as "Tên sản phẩm",
+	p.ProductName, s.CompanyName
+from [Order Details] as od
+inner join Products as p
+on p.ProductID = od.ProductID
+inner join Suppliers as s
+on s.SupplierID = p.SupplierID
+
+--Bài tập 2: left join
+--liệt kê tên khách hàng và tên nhân viên phụ trách của các đơn hàng trong bảng Orders.
+--Bao gồm cả các đơn hàng không có nhân viên phụ trách
+select c.ContactName as "Tên khách hàng",
+	   (e.LastName + ' ' + e.FirstName) as "Tên nhân viên"
+from Orders as o
+left join Customers as c
+on o.CustomerID = c.CustomerID
+left join Employees as e
+on e.EmployeeID = o.EmployeeID
+
+--Bài tập 3: Right join 
+--Liệt kê tên khách hàng và tên nhân viên phụ trách của các đơn hàng trong bảng Orders
+--Bao gồm cả khách hàng không có đơn hàng, Sử dụng Right Join để kết hợp bảng Orders và 
+--bảng Customers
+select c.ContactName as "Tên khách hàng",
+	   (e.LastName + ' ' + e.FirstName) as "Tên khách hàng"
+from Orders as o
+right join Customers as c
+on o.CustomerID = c.CustomerID
+right join Employees as e
+on e.EmployeeID = o.EmployeeID
+
+--Bài tập 4: Full join
+--liệt kê tên danh mục và nhà cung cấp của các sản phẩm trong bảng Products. Bao gồm nhà 
+--cung cấp không có sản phẩm.
+select p.ProductName as "Tên danh mục",
+	   s.CompanyName as "Nhà cung cấp"
+from Products as p
+full join Suppliers as s
+on s.SupplierID = p.SupplierID
+full join Categories as c
+on c.CategoryID = p.CategoryID
---
+++ Code/Subquery.sql
@@ -0,0 +1,62 @@
+﻿--------------------------------------Sub Query/Nest Query---------------------------------
+--Subquery (câu truy vấn con): là một truy vấn select được viết bên trong một truy vấn select,
+--update, insert, hoặc delete khác 
+--Subquery hoạt động như một bảng ảo tạm thời, được sử dụng để trích xuất thông tin từ các bảng
+--hoặc các tập dữ liệu khác trong cùng một câu truy vấn 
+--------------------------------------------------------------------------------------------
+--liệt kê toàn bộ sản phẩm
+select ProductID, ProductName, UnitPrice
+from Products
+
+--tìm giá trung bình
+select avg(UnitPrice) 
+from Products
+
+--lọc những sản phẩm có giá > trung bình
+select ProductID, ProductName, UnitPrice
+from Products
+where UnitPrice > (select avg(UnitPrice) from Products)
+
+--lọc ra nhưng khách hàng có số đơn hàng lớn hơn 10
+select c.CustomerID, c.CompanyName, count(o.OrderID) as TotalOrders
+from Customers as c
+left join Orders as o
+on c.CustomerID = o.CustomerID
+group by c.CustomerID, c.CompanyName
+having count(o.OrderID) > 10
+
+--subquery
+select * 
+from Customers as c
+where c.CustomerID in (
+		select o.CustomerID
+		from Orders as o
+		group by o.CustomerID
+		having count(o.OrderID) > 10
+);
+
+--tính tổng tiền cho từng đơn hàng
+select o.*, (
+	select sum(od.Quantity * od.UnitPrice)
+	from [Order Details] as od
+	where o.OrderID = od.OrderID
+	) as Total
+from Orders as o
+
+--Lọc ra tên sản phẩm và tổng số đơn hàng của sản phẩm
+select p.ProductName as "Tên sản phẩm",
+	   p.ProductID,(
+	select count(*)
+	from [Order Details] as od
+	where od.ProductID = p.ProductID
+	) as "Tổng số đơn hàng"
+from Products as p	
+------------------------------------------------------------------------------------------
+
+--hãy in ra mã đơn hàng, và số lượng sản phẩm của đơn hàng đó
+select o.OrderID as "Mã đơn hàng",(
+	select count(od.Quantity)
+	from [Order Details] as od
+	where od.OrderID = o.OrderID
+	)
+from Orders as o
